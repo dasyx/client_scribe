41a3b210ad8e9246bf09c44b52246ba1747f4872
unknown <seanpnn00@gmail.com>
2023-05-15 15:07:19 +0200

    add prependData files globally

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocessor from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess';
 
 /**  @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocessor(),
+	preprocess: preprocess({
+		scss: {
+			// code below will import theses files globally
+			prependData: `
+			@import 'src/styles/variables.scss';
+			@import 'src/styles/mixins.scss';
+			`
+		}
+	}),
 	kit: {
 		adapter: adapter(),
 	}
